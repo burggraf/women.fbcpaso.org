46efc308bb4cf6a627d2319aa2cea0d25653170a
Mark Burggraf <markb@mantisbible.com>
2022-01-07 08:19:22 -0800

    calendar formatting

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,43 +1,87 @@
-import { IonButton, IonButtons, IonContent, IonDatetime, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { home } from 'ionicons/icons';
-import { useRef } from 'react';
+import {
+	IonButton,
+	IonButtons,
+	IonCol,
+	IonContent,
+	IonDatetime,
+	IonGrid,
+	IonHeader,
+	IonIcon,
+	IonMenuButton,
+	IonPage,
+	IonRow,
+	IonTitle,
+	IonToolbar,
+} from '@ionic/react'
+import { home } from 'ionicons/icons'
+import { useRef, useState } from 'react'
 //import { useParams } from 'react-router';
 //import ExploreContainer from '../components/ExploreContainer';
-import './Calendar.css';
+import './Calendar.css'
 
 const Calendar: React.FC = () => {
-  const cal: any = useRef();
-  // const { name } = useParams<{ name: string; }>();
-  const reset = () => {
-    if (cal === undefined) return;
-    cal.current.reset();
-  };
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>Calendar</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+	const cal: any = useRef()
+	const [selectedDate, setSelectedDate] = useState(new Date().toISOString())
+	// const { name } = useParams<{ name: string; }>();
+	const reset = (e: any) => {
+		console.log('reset called', e)
+		if (cal === undefined) return
+		cal.current.reset()
+	}
+	const updateDate = (e: any) => {
+		console.log('updateDate called', e.detail.value)
+		setSelectedDate(e.detail.value!)
+	}
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonButtons slot='start'>
+						<IonMenuButton />
+					</IonButtons>
+					<IonTitle>Calendar</IonTitle>
+				</IonToolbar>
+			</IonHeader>
 
-      <IonContent className="ion-padding">
+			<IonContent className='ion-padding'>
+				<IonGrid>
+					<IonRow>
+						<IonCol>
+							<IonDatetime
+								ref={cal}
+								presentation='date'
+								//size="cover"
+								//size="fixed"
+								//show-clear-button={true}
+								//showDefaultButtons={true}
+								yearValues='2021,2022,2023,2024'
+								value={selectedDate}
+								mode='ios'
+								onIonChange={updateDate}></IonDatetime>
+							<IonToolbar>
+								<IonButtons>
+									<IonButton fill='clear' onClick={reset}>
+										<IonIcon size='large' color='medium' icon={home}></IonIcon>
+									</IonButton>
+								</IonButtons>
+							</IonToolbar>
+							<div>{new Date(selectedDate).toLocaleDateString()}</div>
+						</IonCol>
+						<IonCol>
+							<div
+								className='ion-padding'
+								style={{ width: '350px', height: '350px', backgroundColor: '#f5f5f5' }}>
+								Date content here<br/>
+                <b>
+                  {new Date(selectedDate).toLocaleDateString()}
+                </b>
+							</div>
+						</IonCol>
+					</IonRow>
+				</IonGrid>
+			</IonContent>
+		</IonPage>
+	)
+}
 
-        <IonDatetime 
-          ref={cal}
-          size="cover" 
-          yearValues="2022,2023,2024">
-        </IonDatetime>
-        <div className="ion-text-center">
-          <IonButton fill="clear" onClick={reset}>
-            <IonIcon size="large" color="medium" icon={home}></IonIcon>
-          </IonButton>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
-};
-
-export default Calendar;
+export default Calendar
